ebbfac8415e99aacac83ebffed8f21aaa3d243e2
Chijioke (Chukwu)  Peter <peterchijioke1@gmail.com>
2023-07-10 16:32:37 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,17 @@ import React, { Fragment } from "react";
 import Routes from "./routes/Main";
 import "./App.scss";
 import Navigations from "./pages/components/Nav/Nav";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function App() {
   return (
     <Fragment>
+      <div className="app-top-nav">
+        <img
+          style={{ width: "40px", height: "40px", marginRight: "2rem" }}
+          src={require("../src/image/icon/bars-solid.svg").default}
+        />
+      </div>
       <div className="container">
         <Navigations />
         <Routes />
